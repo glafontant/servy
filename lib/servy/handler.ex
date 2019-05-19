e8c8fa0094330feea518f5e2f3d2e1926261eaad
Gabe Lafontant <glafonta@gmail.com>
2019-05-19 15:11:07 -0400

    routing with some regex for get requests

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,6 +6,7 @@ defmodule Servy.Handler do
     |> rewrite_path
     |> log
     |> route
+    |> emojify
     |> track
     |> format_response
 	end
@@ -32,35 +33,45 @@ defmodule Servy.Handler do
 
   def track(conv), do: conv
 
-  # We want the request for boston_teams to be handled
-  # by /boston_sports_teams
-  def rewrite_path(%{ path: "/boston_teams" } = conv) do
-    %{ conv | path: "/boston_sports_teams" }
+  def rewrite_path(%{ path: path } = conv) do
+    regex = ~r{\/(?<route>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path_captures(conv, %{"route" => route, "id" => id}) do
+    %{ conv | path: "/#{route}/#{id}" }
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
 
   def log(conv), do: IO.inspect conv
 
-  def route(%{method: method, path: "/boston_sports_teams"} = conv) do
+  def route(%{method: "GET", path: "/boston_sports_teams"} = conv) do
      %{ conv | status: 200, resp_body: "Celtics, Patriots, Bruins, Red Sox" }
   end
 
-  def route(%{method: method, path: "/patriots"} = conv) do
+  def route(%{method: "GET", path: "/patriots"} = conv) do
     %{ conv | status: 200, resp_body: "Kraft, Belichik, Brady" }
   end
 
-  def route(%{method: method, path: "/patriots/" <> id} = conv) do
+  def route(%{method: "GET", path: "/patriots/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "Patriot #{id}"}
   end
 
-  def route(%{method: method, path: "/patriots/" <> id} = conv) do
+  def route(%{method: "DELETE", path: "/patriots/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "Patriot #{id} removed"}
   end
 
   def route(%{path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
+
+  def emojify(%{resp_body: resp_body, status: 200} = conv) do
+    %{ conv | resp_body: resp_body <> " :D" }
+  end
+
+  def emojify(conv), do: conv
 
   def format_response(conv) do
     """
@@ -149,6 +160,32 @@ IO.puts response
 
 request = """
 GET /boston_teams HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /patriots?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /patriots?id=2 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
