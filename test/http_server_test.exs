094aaf1cb76b755f7c58ba15f31f4814f56419c8
Gabe Lafontant <glafonta@gmail.com>
2019-08-18 17:48:48 -0400

    better improved specs for httpserver

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -6,22 +6,20 @@ defmodule HttpServerTest do
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    max_concurrents = 5
-    caller = self()
+    urls = [
+      "http://localhost:4000/boston_sports_teams",
+      "http://localhost:4000/patriots",
+      "http://localhost:4000/patriots/1",
+      "http://localhost:4000/api/patriots"
+    ]
 
-    for _ <- 1..max_concurrents do
-      spawn(fn ->
-        {:ok, response} = HTTPoison.get("localhost:4000/boston_sports_teams")
-        send(caller, {:ok, response})
-      end)
-    end
+    urls
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&assert_successful_response/1)
+  end
 
-    for _ <- 1..max_concurrents do
-      receive do
-        {:ok, response} ->
-         assert response.status_code == 200
-         assert response.body == "Celtics, Patriots, Bruins, Red Sox"
-      end
-    end
+  defp assert_successful_response({:ok, response}) do
+    assert response.status_code == 200
   end
 end
