09451227540a55cd8e53a8ada723cbcc33d181cb
Gabe Lafontant <glafonta@gmail.com>
2019-09-29 15:27:51 -0400

    more fun understanding generic server interface

--- lib/servy/two_hundred_counter.ex
+++ lib/servy/two_hundred_counter.ex
@@ -1,49 +1,82 @@
-defmodule Servy.TwoHundredCounter do
+defmodule Servy.GenericsServer do
 
-  @name __MODULE__
-
-  #Client Interface
-
-  def start(initial_state \\ %{}) do
-    IO.puts "\nWaiting for a message"
-    pid = spawn(@name, :listen_loop, [initial_state])
-    Process.register pid, @name
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register pid, name
     pid
   end
 
+  def call(pid, message) do
+    send pid, {:call, self(), message}
+    receive do {:response, response} -> response end
+  end
+
+  def cast(pid, message) do
+    send pid, {:cast, message}
+  end
+
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send sender, {:response, response}
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state, callback_module)
+    end
+  end
+end
+
+
+defmodule Servy.TwoHundredCounter do
+  alias Servy.GenericsServer
+
+  @name __MODULE__
+
+  def start do
+    GenericsServer.start(__MODULE__, %{}, @name)
+  end
+
+  # Client Requests
+
   def bump_count(path) do
-    send @name, {self(), :bump_count, path}
-    receive do {:response, :ok} -> :ok end
+    GenericsServer.call @name, {:bump_count, path}
   end
 
 
   def get_count(path) do
-    send @name, {self(), :get_count, path}
-    receive do {:response, count} -> count end
+    GenericsServer.call @name, {:get_count, path}
   end
 
   def get_counts do
-    send @name, {self(), :get_counts}
-    receive do {:response, counts} -> counts end
+    GenericsServer.call @name, :get_counts
   end
 
-  #Server Interface
-  def listen_loop(state) do
-    receive do
-      {sender, :bump_count, path} ->
-        new_state = Map.update(state, path, 1, &(&1 + 1))
-        send sender, {:response, :ok}
-        listen_loop(new_state)
-      {sender, :get_count, path} ->
-        count = Map.get(state, path, 0)
-        send sender, {:response, count}
-        listen_loop(state)
-      {sender, :get_counts} ->
-        send sender, {:response, state}
-        listen_loop(state)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect unexpected}"
-        listen_loop(state)
-    end
+  def clear_cache do
+    GenericsServer.cast @name, :clear
+  end
+
+  #Server Callbacks
+
+  def handle_call(:get_counts, state) do
+    {state, state}
+  end
+
+  def handle_call({:get_count, path}, state) do
+    count = Map.get(state, path, 0)
+    {count, state}
+  end
+
+  def handle_call({:bump_count, path}, state) do
+    new_state = Map.update(state, path, 1, &(&1 + 1))
+    {:ok, new_state}
+  end
+
+  def handle_cast(:clear, _state) do
+    %{}
   end
 end
